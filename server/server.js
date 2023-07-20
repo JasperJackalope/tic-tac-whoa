4213bad298a819da43cb72ec03d854eefdeb67c8
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-07-20 16:47:53 -0700

    Updated Code

--- server/server.js
+++ server/server.js
@@ -16,9 +16,16 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+//   server.applyMiddleware({ app });
+// }
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-  server.applyMiddleware({ app });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
 
 app.get('*', (req, res) => {
