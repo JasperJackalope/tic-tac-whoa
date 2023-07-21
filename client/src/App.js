960b61c1a2bdf6d3b2cc1c8b31ee05e640ad728f
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-07-20 19:06:23 -0700

    Heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,84 @@
+// import React from 'react';
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Login from "./components/Login";
+// import Dashboard from "./components/Dashboard/Dashboard.js";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import GamePage from './components/Game/Game';
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./App2.css";
+
+// // function App() {
+// //   const [route, setRoute] = React.useState(window.location.pathname);
+// //   const httpLink = createHttpLink({
+// //     uri: '/graphql',
+// //   });
+
+//   function App() {
+//     const [setRoute] = React.useState(window.location.pathname);
+//     const httpLink = createHttpLink({
+//       uri: '/graphql',
+//     });
+  
+//   // Construct request middleware that will attach the JWT token to every request as an `authorization` header
+//   const authLink = setContext((_, { headers }) => {
+//     // get the authentication token from local storage if it exists
+//     const token = localStorage.getItem('id_token');
+//     // return the headers to the context so httpLink can read them
+//     return {
+//       headers: {
+//         ...headers,
+//         authorization: token ? `Bearer ${token}` : '',
+//       },
+//     };
+//   });
+  
+//   const client = new ApolloClient({
+//     // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+//     link: authLink.concat(httpLink),
+//     cache: new InMemoryCache(),
+//   });
+
+//   React.useEffect(() => {
+//     const handleRouteChange = () => {
+//       setRoute(window.location.pathname);
+//     };
+
+//     window.addEventListener("popstate", handleRouteChange);
+
+//     return () => {
+//       window.removeEventListener("popstate", handleRouteChange);
+//     };
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <Header />
+//       <main>
+//         <ApolloProvider client={client}>
+//           <Router>
+//             <Routes>
+//               <Route path="/" element={<Login />} />
+//               <Route path="/game" element={<GamePage />} />
+//               <Route path="/dashboard" element={<Dashboard />} />
+//             </Routes>
+//           </Router>
+//         </ApolloProvider>
+//       </main>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
 import {
   ApolloClient,
@@ -15,18 +96,12 @@ import GamePage from './components/Game/Game';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App2.css";
 
-// function App() {
-//   const [route, setRoute] = React.useState(window.location.pathname);
-//   const httpLink = createHttpLink({
-//     uri: '/graphql',
-//   });
+function App() {
+  const [route, setRoute] = React.useState(window.location.pathname);
+  const httpLink = createHttpLink({
+    uri: '/graphql',
+  });
 
-  function App() {
-    const [setRoute] = React.useState(window.location.pathname);
-    const httpLink = createHttpLink({
-      uri: '/graphql',
-    });
-  
   // Construct request middleware that will attach the JWT token to every request as an `authorization` header
   const authLink = setContext((_, { headers }) => {
     // get the authentication token from local storage if it exists
@@ -78,3 +153,4 @@ import "./App2.css";
 }
 
 export default App;
+
