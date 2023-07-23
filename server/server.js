8e1181d92222f5bea2861e5acade7cff82c35e85
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-07-22 18:56:12 -0700

    Updated Deploy Credentials

--- server/server.js
+++ server/server.js
@@ -41,6 +41,7 @@
 
 // startApolloServer(typeDefs, resolvers);
 
+require("dotenv").config();
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
