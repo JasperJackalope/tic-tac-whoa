72528b562aaa0f6f4b007f175b5951d7b59b9baa
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-07-22 17:52:25 -0700

    Updated Server Connections

--- server/server.js
+++ server/server.js
@@ -1,3 +1,46 @@
+// const express = require('express');
+// const { ApolloServer } = require('apollo-server-express');
+// const path = require('path');
+// const { authMiddleware } = require('./utils/auth');
+// const { typeDefs, resolvers } = require('./schemas');
+// const db = require('./config/connection');
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware,
+// });
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+//   app.use(express.static(path.join(__dirname, '../client/public')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/public/index.html'));
+//   });
+// }
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/public/index.html'));
+// });
+
+// const startApolloServer = async (typeDefs, resolvers) => {
+//   await server.start();
+//   server.applyMiddleware({ app });
+  
+//   db.once('open', () => {
+//     app.listen(PORT, () => {
+//       console.log(`API server running on port ${PORT}!`);
+//       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+//     });
+//   });
+// };
+
+// startApolloServer(typeDefs, resolvers);
+
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
@@ -16,21 +59,15 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-//   server.applyMiddleware({ app });
-// }
-
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  // Serve static assets from the client/build directory
   app.use(express.static(path.join(__dirname, '../client/build')));
+
+  // Catch-all route to serve the React app's index.html
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
