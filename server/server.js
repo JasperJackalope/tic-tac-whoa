cd321855ecb6dc9e5ea6e2f52ef2537045d51d2d
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-07-22 18:37:02 -0700

    Updated connection

--- server/server.js
+++ server/server.js
@@ -60,10 +60,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-  // Serve static assets from the client/build directory
   app.use(express.static(path.join(__dirname, '../client/public')));
-
-  // Catch-all route to serve the React app's index.html
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
   });
