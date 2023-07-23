1302ab7d024e24444692a9b960c4e3eb385c0577
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-07-23 15:23:42 -0700

    Mongo DB Cred

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tic-tac-whoa',
+  process.env.MONGODB_URI || 'mongodb+srv://djbske:JaXTAWHmiBpvScQ9@cluster0.rt27npu.mongodb.net/?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
